5880d12ded97db007486366c8aa8caea504c15ed
Janus <Janus.Haung@hualiteq.com>
2025-08-25 16:46:57 +0800

    Initial Commit

---
+++ bagitem/bagitemApp.swift
@@ -0,0 +1,17 @@
+//
+//  bagitemApp.swift
+//  bagitem
+//
+//  Created by Hualiteq International on 2025/8/25.
+//
+
+import SwiftUI
+
+@main
+struct bagitemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
